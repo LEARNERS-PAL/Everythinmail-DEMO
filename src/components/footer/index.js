e0fa8f3bba841aa6f117c827b6102457536a8866
baabashinelle <baabadampare@gmail.com>
2023-07-22 21:06:47 +0000

    edit footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='flex justify-center font-inter lg:px-14 px-8 text-gray-600 py-8'>2023, All Rights Reserved. EverythingMail</section>
+    <section className='flex justify-center font-inter lg:px-14 px-8 text-gray-600 py-8'>2023, All Rights Reserved. EverythingMail API</section>
   )
 }
 
